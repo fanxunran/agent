b615cee895b6776592b344d0b8c51befdf0e8220
fxr <fxr@gitlab.localhost>
2019-03-08 11:56:10 +0800

    引入filter库

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './style/reset.css'
 import App from './App'
 import router from './router'
+import filters from './utils/filters';
+
 //引入echarts库
 import echarts from 'echarts'
 Vue.prototype.$echarts = echarts
+
+// 注册过滤器
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key]);
+});
 
 //配置代理引入文件
 // import axios from 'axios'
